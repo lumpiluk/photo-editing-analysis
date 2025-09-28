b3d26c99851e5106415559aca42ba3a69bd31dfd
Lukas Stratmann <stratmann@ccs-labs.org>
2025-09-28 22:10:34 +0200

    Refactor: Plotting functions don't assume raw vs edited

--- analysis.py
+++ analysis.py
@@ -25,35 +25,29 @@ def main():
     parser.add_argument(
         "--delta-plot",
         type=pathlib.Path,
-        required=True,
         help="File name of the plot of time between "
              "raw files and edited files.",
     )
     parser.add_argument(
         "--sessions-plot",
         type=pathlib.Path,
-        required=True,
         help="File name of the plot of session durations.",
     )
     parser.add_argument(
         "--focal-lengths-plot",
         type=pathlib.Path,
-        required=False,
     )
     parser.add_argument(
         "--exposure-times-plot",
         type=pathlib.Path,
-        required=False,
     )
     parser.add_argument(
         "--apertures-plot",
         type=pathlib.Path,
-        required=False,
     )
     parser.add_argument(
         "--isos-plot",
         type=pathlib.Path,
-        required=False,
     )
     args = parser.parse_args()
 
@@ -68,29 +62,32 @@ def main():
         for file in folder.glob("converted*/*.jpg")
     ]
 
-    mtimes_raw = sorted(collect_file_stats(files=raw_files))
-    mtimes_edit = sorted(collect_file_stats(files=edited_files))
+    if args.delta_plot or args.sessions_plot:
+        mtimes_raw = sorted(collect_file_stats(files=raw_files))
+        mtimes_edit = sorted(collect_file_stats(files=edited_files))
 
-    plot_time_between_photos(
-        mtimes_raw=mtimes_raw,
-        mtimes_edit=mtimes_edit,
-        out_filename=args.delta_plot,
-    )
+        if args.delta_plot:
+            plot_time_between_photos(
+                mtimes_list=[mtimes_raw, mtimes_edit],
+                mtimes_labels=["Photos shot", "Photos edited"],
+                out_filename=args.delta_plot,
+            )
 
-    sessions_raw = get_sessions_from_time_series(
-        timestamps_sec=mtimes_raw,
-        min_break_between_sessions_sec=60 * 30,
-    )
-    sessions_edit = get_sessions_from_time_series(
-        timestamps_sec=mtimes_edit,
-        min_break_between_sessions_sec=60 * 30,
-    )
-
-    plot_sessions(
-        sessions_raw=sessions_raw,
-        sessions_edit=sessions_edit,
-        out_filename=args.sessions_plot,
-    )
+        if args.sessions_plot:
+            sessions_raw = get_sessions_from_time_series(
+                timestamps_sec=mtimes_raw,
+                min_break_between_sessions_sec=60 * 30,
+            )
+            sessions_edit = get_sessions_from_time_series(
+                timestamps_sec=mtimes_edit,
+                min_break_between_sessions_sec=60 * 30,
+            )
+            plot_sessions(
+                sessions_list=[sessions_raw, sessions_edit],
+                sessions_labels=["Photo shoot sessions", "Editing sessions"],
+                out_filename=args.sessions_plot,
+                show_info_text=True,
+            )
 
     if (
             args.focal_lengths_plot
@@ -104,32 +101,32 @@ def main():
 
         if args.focal_lengths_plot:
             plot_metadata(
-                metadata_raw=metadata_raw,
-                metadata_edited=metadata_edited,
+                metadata_lists=[metadata_raw, metadata_edited],
+                metadata_labels=["Raw photos", "Edited photos"],
                 tag="EXIF:FocalLength",
                 xlabel="Focal length in mm",
                 out_filename=args.focal_lengths_plot,
             )
         if args.exposure_times_plot:
             plot_metadata(
-                metadata_raw=metadata_raw,
-                metadata_edited=metadata_edited,
+                metadata_lists=[metadata_raw, metadata_edited],
+                metadata_labels=["Raw photos", "Edited photos"],
                 tag="EXIF:ExposureTime",
                 xlabel="Exposure time in seconds",
                 out_filename=args.exposure_times_plot,
             )
         if args.apertures_plot:
             plot_metadata(
-                metadata_raw=metadata_raw,
-                metadata_edited=metadata_edited,
+                metadata_lists=[metadata_raw, metadata_edited],
+                metadata_labels=["Raw photos", "Edited photos"],
                 tag="EXIF:FNumber",
                 xlabel="Aperture (F-number)",
                 out_filename=args.apertures_plot,
             )
         if args.isos_plot:
             plot_metadata(
-                metadata_raw=metadata_raw,
-                metadata_edited=metadata_edited,
+                metadata_lists=[metadata_raw, metadata_edited],
+                metadata_labels=["Raw photos", "Edited photos"],
                 tag="EXIF:ISO",
                 xlabel="ISO",
                 out_filename=args.isos_plot,
@@ -160,14 +157,16 @@ def get_sessions_from_time_series(
 
 
 def plot_time_between_photos(
-    mtimes_raw: Iterable[float],
-    mtimes_edit: Iterable[float],
+    mtimes_list: list[Iterable[float]],
+    mtimes_labels: list[str],
     out_filename: pathlib.Path,
 ):
     # Compute delta times (first item will be NaN):
-    dt_raw = pd.Series(mtimes_raw).diff()
-    dt_edit = pd.Series(mtimes_edit).diff()
-    df = pd.DataFrame({"Photos shot": dt_raw, "Photos edited": dt_edit})
+    df = pd.DataFrame({
+        label: pd.Series(mtimes).diff()
+        for label, mtimes
+        in zip(mtimes_labels, mtimes_list)
+    })
 
     fig, ax = plt.subplots(figsize=(4, 3))
     sns.ecdfplot(
@@ -182,40 +181,49 @@ def plot_time_between_photos(
 
 
 def plot_sessions(
-    sessions_raw: Iterable[float],
-    sessions_edit: Iterable[float],
+    sessions_list: list[Iterable[float]],
+    sessions_labels: list[str],
     out_filename: pathlib.Path,
+    show_info_text=True,  # Currently only for <= 2 collections of sessions
 ):
-    sessions_raw_minutes = pd.Series(sessions_raw) / 60
-    sessions_edit_minutes = pd.Series(sessions_edit) / 60
+    sessions_list_minutes = [
+        pd.Series(sessions) / 60
+        for sessions in sessions_list
+    ]
     df = pd.DataFrame({
-        "Photo shoot sessions": sessions_raw_minutes,
-        "Editing sessions": sessions_edit_minutes,
+        label: sessions
+        for label, sessions
+        in zip(sessions_labels, sessions_list_minutes)
     })
 
-    fig, ax = plt.subplots(figsize=(4, 3.5))
+    fig, ax = plt.subplots(figsize=(
+        4,
+        3.5 if show_info_text else 3
+    ))
     sns.ecdfplot(
         data=df,
         ax=ax,
     )
-    # ax.set_xlim(left=0, right=
     ax.set_xlabel("Session duration in minutes")
-    fig.text(
-        .05,
-        .05,
-        f"{len(sessions_raw_minutes)} photo shoot sessions, "
-        # f"{datetime.timedelta(minutes=sum(sessions_raw_minutes))}\n"
-        f"{sum(sessions_raw_minutes) / 60:.2f} hours total\n"
-        f"{len(sessions_edit_minutes)} editing sessions, "
-        # f"{datetime.timedelta(minutes=sum(sessions_edit_minutes))}",
-        f"{sum(sessions_edit_minutes) / 60:.2f} hours total",
-        ha="left",
-        va="bottom",
-        fontsize=10,
-        transform=fig.transFigure,
-    )
+    if show_info_text:
+        fig.text(
+            .05,
+            .05,
+            "\n".join(
+                f"{label} total: {len(sessions_minutes)}, "
+                f"duration: {sum(sessions_minutes) / 60:.2f} hours"
+                # f"{datetime.timedelta(minutes=sum(sessions_minutes))}",
+                for label, sessions_minutes
+                in zip(sessions_labels, sessions_list_minutes)
+            ),
+            ha="left",
+            va="bottom",
+            fontsize=9,
+            transform=fig.transFigure,
+        )
     fig.tight_layout()
-    fig.subplots_adjust(bottom=.30)
+    if show_info_text:
+        fig.subplots_adjust(bottom=.30)
     fig.savefig(out_filename)
 
 
@@ -227,17 +235,20 @@ def get_metadata(
 
 
 def plot_metadata(
-    metadata_raw: list[dict],
-    metadata_edited: list[dict],
+    metadata_lists: list[list[dict]],
+    metadata_labels: list[str],
     tag: str,
     xlabel: str,
     out_filename: pathlib.Path,
 ):
-    data_raw = sorted([it[tag] for it in metadata_raw])
-    data_edit = sorted([it[tag] for it in metadata_edited])
+    assert len(metadata_lists) == len(metadata_labels)
+
     df = pd.DataFrame({
-        "Raw photos": pd.Series(data_raw),
-        "Edited photos": pd.Series(data_edit),
+        metadata_label: pd.Series(sorted([
+            it[tag] for it in metadata_list
+        ]))
+        for metadata_label, metadata_list
+        in zip(metadata_labels, metadata_lists)
     })
 
     fig, ax = plt.subplots(figsize=(4, 3))
